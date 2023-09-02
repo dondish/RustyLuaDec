ca442f20e059a932aa53f38e0864659e5b52f8a0
dondish <odedshapira1@protonmail.com>
2023-09-02 16:18:31 +0300

    Add debug info unit tests

--- src/common_structs/debug_info.rs
+++ src/common_structs/debug_info.rs
@@ -61,3 +61,52 @@ impl DebugInfo {
         )(input)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{AbsLineInfo, DebugInfo, LocalVar};
+
+    #[test]
+    fn test_abs_line_info_parsing() {
+        let data: [u8; 0x03] = [0x01, 0x80, 0xAD];
+        let abs_line_info_res = AbsLineInfo::parse(&data);
+        assert!(abs_line_info_res.is_ok());
+        assert_eq!(
+            abs_line_info_res.unwrap().1,
+            AbsLineInfo { pc: 128, line: 45 }
+        )
+    }
+
+    #[test]
+    fn test_local_var_parsing() {
+        let data: [u8; 0x05] = [0x82, 0x72, 0x82, 0x01, 0xC0];
+        let local_var_res = LocalVar::parse(&data);
+        assert!(local_var_res.is_ok());
+        assert_eq!(
+            local_var_res.unwrap().1,
+            LocalVar {
+                name: "r".to_string(),
+                start_pc: 2,
+                end_pc: 192
+            }
+        )
+    }
+
+    #[test]
+    fn test_debug_info_parsing() {
+        let data: [u8; 0x0D] = [
+            0x87, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x80, 0x80, 0x81, 0x82, 0x78,
+        ];
+        let debug_info_res = DebugInfo::parse(&data);
+        assert!(debug_info_res.is_ok());
+        assert_eq!(
+            debug_info_res.unwrap().1,
+            DebugInfo {
+                line_info: vec![1, 0, 0, 0, 0, 0, 1],
+                abs_line_info: vec![],
+                local_vars: vec![],
+                upvalue_names: vec!["x".to_string()],
+            }
+        )
+    }
+}
