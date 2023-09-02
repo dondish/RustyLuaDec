2c3df8951625aff85ca4535fb95b0b6451421551
dondish <odedshapira1@protonmail.com>
2023-09-02 14:55:26 +0300

    Add unit test to upvalue.rs

--- src/common_structs/upvalue.rs
+++ src/common_structs/upvalue.rs
@@ -35,3 +35,24 @@ impl Upvalue {
         )(input)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::common_structs::variable_kind::VariableKind;
+
+    use super::Upvalue;
+
+    #[test]
+    fn test_upvalue_parsing() {
+        let data: [u8; 0x03] = [
+            0x01, 0x00, 0x00
+        ];
+        let upvalue = Upvalue::parse(&data[..]);
+        assert!(upvalue.is_ok());
+        assert_eq!(upvalue.unwrap().1, Upvalue {
+            in_stack: true,
+            index: 0,
+            kind: VariableKind::Regular
+        })
+    }
+}
